ae5346ff83b2bf06de7eb2b9b7878c58b085dd54
Abe <abe_vos@msn.com>
2019-05-05 22:10:53 +0200

    Start VAE implementation

--- assignment_3/code/a3_vae_template.py
+++ assignment_3/code/a3_vae_template.py
@@ -13,6 +13,12 @@ class Encoder(nn.Module):
     def __init__(self, hidden_dim=500, z_dim=20):
         super().__init__()
 
+        self.linear1 = nn.Linear(28 ** 2, hidden_dim)
+        self.linear_mu = nn.Linear(hidden_dim, z_dim)
+        self.linear_std = nn.Linear(hidden_dim, z_dim)
+
+        self.activation = nn.ReLU()
+
     def forward(self, input):
         """
         Perform forward pass of encoder.
@@ -20,8 +26,10 @@ class Encoder(nn.Module):
         Returns mean and std with shape [batch_size, z_dim]. Make sure
         that any constraints are enforced.
         """
-        mean, std = None, None
-        raise NotImplementedError()
+        input = self.activation(self.linear1(input))
+
+        mean = self.linear_mu(input)
+        std = self.activation(self.linear_std(input))
 
         return mean, std
 
@@ -31,14 +39,19 @@ class Decoder(nn.Module):
     def __init__(self, hidden_dim=500, z_dim=20):
         super().__init__()
 
+        self.linear1 = nn.Linear(z_dim, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, 28 ** 2)
+
+        self.activation = nn.ReLU()
+
     def forward(self, input):
         """
         Perform forward pass of encoder.
 
         Returns mean with shape [batch_size, 784].
         """
-        mean = None
-        raise NotImplementedError()
+        mean = self.activation(self.linear1(input))
+        mean = self.activation(self.linear2(mean))
 
         return mean
 
@@ -52,13 +65,33 @@ class VAE(nn.Module):
         self.encoder = Encoder(hidden_dim, z_dim)
         self.decoder = Decoder(hidden_dim, z_dim)
 
+        self.recon_loss = nn.CrossEntropyLoss()
+
     def forward(self, input):
         """
         Given input, perform an encoding and decoding step and return the
         negative average elbo for the given batch.
         """
-        average_negative_elbo = None
-        raise NotImplementedError()
+        def kl_divergence(mean, std):
+            """
+            Compute the KL-divergence between the predicted mean and standard
+            deviation and the standard normal distribution.
+            """
+            return torch.log(1 / torch.sqrt(std)) \
+                    + (std + torch.pow(mean, 2)) \
+                    / (2 * std) \
+                    - 0.5
+
+        mean, std = self.encoder(input)
+
+        noise = torch.randn(std.size())
+        z = noise * std + mean
+
+        output = self.decoder(z)
+
+        average_negative_elbo = self.recon_loss(output, input) \
+                - kl_divergence(mean, std)
+        average_negative_elbo = - average_negative_elbo.mean(0)
         return average_negative_elbo
 
     def sample(self, n_samples):
